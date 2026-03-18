2d087885702c5d4f4703a252329735f9a0e1ec41
teklaturmanidze92-prog <teklaturmanidze92@gmail.com>
2026-03-19 00:10:16 +0400

    Delete rndm.js

--- rndm.js
+++
@@ -1,7 +0,0 @@
-const request = fetch("https://uselessfacts.jsph.pl/api/v2/facts/random")
-  .then((response) => response.json())
-  .then((data) => {
-    document.getElementById("fact-text").textContent = data.text;
-  });
-
-console.log(request);
